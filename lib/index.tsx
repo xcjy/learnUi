cf10745dcd266b45483c1644ce3db7796b6270d5
xcjy <chenjianyuan2@outlook.com>
2019-11-23 22:59:21 +0800

    npm发布测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,18 +1,2 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-// import Button from './button';
-import Icon from './icon/icon';
 
-const ClickHandler:React.MouseEventHandler=(e)=>{
-    console.log(e.target)
-}
-
-ReactDom.render(<div>
-    <Icon name="wechat" onClick={ClickHandler}
-      className="qqq"
-      onMouseLeave={()=>{console.log('leave')}}
-      onMouseEnter={ ()=>{console.log('enter')}
-    } 
-    />
-
-</div>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon';
