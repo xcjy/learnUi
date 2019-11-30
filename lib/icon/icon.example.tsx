94754dde6ef1474845ed399e0abd4eb35f606408
xcjy <chenjianyuan2@outlook.com>
2019-11-30 22:53:40 +0800

    add example.tsx

---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from './icon'
+const IconExample:React.FunctionComponent=()=>{
+    return(
+        <div> 
+            <Icon name="wechat"/>
+            <Icon name="alipay"/>
+            <Icon name="qq"/>
+
+        </div>
+    )
+}
+export default IconExample;
